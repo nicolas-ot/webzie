e7fe730e7e2c4cc2c71411493da53021fdab7ef6
FelicianoSurya <suryamarcello@gmail.com>
2021-08-04 16:24:57 +0700

    Add SignUp page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './app.scss';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Auth from './pages/Auth/Auth';
 import Main from './pages/Main/Main';
+import SignUp from './pages/Auth/signUp';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
           </Route>
           <Route path='/login'>
             <Auth />
+          </Route>
+          <Route path="/sign-up">
+            <SignUp />
           </Route>
         </Switch>
         {/* {react router: Landing, Main, SignIn/Up} */}
