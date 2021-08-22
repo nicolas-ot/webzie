dcce854794a5bcbbeb858fe5fa23524db9fdb033
2001-otter <nicolastheodarus@gmail.com>
2021-08-22 22:34:10 +0200

    updating webinar table

--- src/components/WebinarTable/WebinarDetails/WebinarDetails.tsx
+++ src/components/WebinarTable/WebinarDetails/WebinarDetails.tsx
@@ -1,4 +1,6 @@
 import './webinarDetails.scss';
+import dateformat from 'dateformat';
+import Button from '../../../elements/Button/Button';
 
 interface WebinarDetailsProps {
   poster: string;
@@ -6,28 +8,39 @@ interface WebinarDetailsProps {
   time: string;
   category: string;
   host: string;
+  title: string;
 }
 
 const WebinarDetails: React.FC<WebinarDetailsProps> = ({
+  title,
   poster,
   date,
   time,
   category,
   host,
 }) => {
+  const now = new Date();
   return (
-    <>
-      <img
-        className='webinar-table-details-poster'
-        src={poster}
-        alt='poster'
-      ></img>
-      <div>1</div>
-      {/* <div>{date}</div> */}
-      <div>{time}</div>
-      <div>{category}</div>
-      <div>{host}</div>
-    </>
+    <div className='webinar-table-details-wrapper row'>
+      <div className='image-and-title column'>
+        <img
+          className='webinar-table-details-poster'
+          src={poster}
+          alt='poster'
+        ></img>
+        <div className='title'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, ex.
+        </div>
+      </div>
+      <div className='column'>{dateformat(now, 'dddd, mmmm dd')}</div>
+      <div className='column'>{time}</div>
+      <div className='column category'>{category}</div>
+      <div className='column'>{host}</div>
+      <div className='button-wrapper'>
+        <Button onClick={() => null}>Download</Button>
+        <Button onClick={() => null}>View</Button>
+      </div>
+    </div>
   );
 };
 
--- src/components/WebinarTable/WebinarTable.tsx
+++ src/components/WebinarTable/WebinarTable.tsx
@@ -8,6 +8,7 @@ import webinarData from '../../data/static/webinar_mock.js';
 const WebinarTable = () => {
   const webinarDetailsComponent = webinarData.map((webinar) => (
     <WebinarDetails
+      title={webinar.title}
       poster={webinar.poster}
       date={webinar.date}
       host={webinar.host}
@@ -19,11 +20,13 @@ const WebinarTable = () => {
     <div className='webinar-table-wrapper'>
       <SearchBar withResult={false} />
       <div className='webinar-table-information-wrapper'>
-        <h6>Name</h6>
-        <h6>Date</h6>
-        <h6>Time</h6>
-        <h6>Category</h6>
-        <h6>Host</h6>
+        <div className='table-header row'>
+          <h6 className='column name'>Name</h6>
+          <h6 className='column'>Date</h6>
+          <h6 className='column'>Time</h6>
+          <h6 className='column'>Category</h6>
+          <h6 className='column'>Host</h6>
+        </div>
         {webinarDetailsComponent}
       </div>
     </div>
