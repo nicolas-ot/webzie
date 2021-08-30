5bbdfa9382c7044af84af73422c0765fea1dcffb
2001-otter <nicolastheodarus@gmail.com>
2021-08-30 19:23:33 +0200

    before adding prevStep keyframe

--- src/pages/RegisterWebinar/ProgressBar/ProgressBar.tsx
+++ src/pages/RegisterWebinar/ProgressBar/ProgressBar.tsx
@@ -1,68 +1,85 @@
 import { useRef } from 'react';
 import './progressBar.scss';
-const ProgressBar = () => {
+
+import { Steps } from '../RegisterWebinar';
+import { useEffect } from 'react';
+
+interface ProgressBarProps {
+  step: number;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ step }) => {
   const step1 = useRef<HTMLDivElement>(null);
   const step2 = useRef<HTMLDivElement>(null);
   const step3 = useRef<HTMLDivElement>(null);
   const step4 = useRef<HTMLDivElement>(null);
-  let step = 'step1';
-
-  setTimeout(() => console.log(step1), 5000);
-
-  function next() {
-    if (step1.current && step2.current && step3.current && step4.current) {
-      if (step === 'step1') {
-        step = 'step2';
+  const step5 = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (
+      step1.current &&
+      step2.current &&
+      step3.current &&
+      step4.current &&
+      step5.current
+    ) {
+      if (step === Steps.STEP_1) {
+        step1.current.classList.add('is-active');
+        step1.current.classList.remove('is-complete');
+        step2.current.classList.remove('is-active');
+      } else if (step === Steps.STEP_2) {
         step1.current.classList.remove('is-active');
         step1.current.classList.add('is-complete');
         step2.current.classList.add('is-active');
-      } else if (step === 'step2') {
-        step = 'step3';
+        step2.current.classList.remove('is-complete');
+        step3.current.classList.remove('is-active');
+      } else if (step === Steps.STEP_3) {
         step2.current.classList.remove('is-active');
         step2.current.classList.add('is-complete');
         step3.current.classList.add('is-active');
-      } else if (step === 'step3') {
-        step = 'step4d';
-        step3.current.classList.remove('is-active');
-        step3.current.classList.add('is-complete');
-        step4.current.classList.add('is-active');
-      } else if (step === 'step4d') {
-        step = 'complete';
+        step3.current.classList.remove('is-complete');
+        step4.current.classList.remove('is-active');
+      } else if (step === Steps.STEP_4) {
+        // step3.current.classList.remove('is-active');
+        // step3.current.classList.add('is-complete');
+        // step4.current.classList.add('is-active');
+        // step4.current.classList.remove('is-complete');
+        // step5.current.classList.remove('is-active');
+
+        step4.current.classList.add('is-not-complete');
+        step4.current.classList.remove('is-complete');
+        step5.current.classList.remove('is-active');
+      } else if (step === Steps.STEP_5) {
         step4.current.classList.remove('is-active');
         step4.current.classList.add('is-complete');
-      } else if (step === 'complete') {
-        step = 'step1';
-        step4.current.classList.remove('is-complete');
-        step3.current.classList.remove('is-complete');
-        step2.current.classList.remove('is-complete');
-        step1.current.classList.remove('is-complete');
-        step1.current.classList.add('is-active');
+        step5.current.classList.add('is-active');
       }
     }
-  }
+  }, [step]);
   return (
     <div>
       <div className='progress'>
         <div className='progress-track'></div>
 
         <div id='step1' className='progress-step' ref={step1}>
-          Step One
+          Information
         </div>
 
         <div id='step2' ref={step2} className='progress-step'>
-          Step Two
+          Description
         </div>
 
         <div id='step3' ref={step3} className='progress-step'>
-          Step Three
+          Poster
         </div>
 
         <div id='step4' ref={step4} className='progress-step'>
-          Complete
+          E-Certificate
+        </div>
+
+        <div id='step5' ref={step5} className='progress-step'>
+          Publishing
         </div>
       </div>
-
-      <button onClick={() => next()}>Next Step</button>
     </div>
   );
 };
--- src/pages/RegisterWebinar/RegisterWebinar.tsx
+++ src/pages/RegisterWebinar/RegisterWebinar.tsx
@@ -1,10 +1,80 @@
+import styles from './registerWebinar.module.scss';
+import image from '../../assets/images/images/register-webinar.png';
+
+import { useState } from 'react';
 import ProgressBar from './ProgressBar/ProgressBar';
+import Button from '../../elements/Button/Button';
+import { useHistory } from 'react-router';
+
+export enum Steps {
+  STEP_1 = 1,
+  STEP_2,
+  STEP_3,
+  STEP_4,
+  STEP_5,
+}
 
 const RegisterWebinar = () => {
+  const [step, setStep] = useState(1);
+  const [forward, setForward] = useState(true);
+  const history = useHistory();
+
+  let buttons;
+
+  const handleCancel = () => {
+    history.push('/');
+  };
+
+  const handleSubmit = () => {
+    history.push('/');
+  };
+
+  const handleNext = () => {
+    setStep((prevStep) => prevStep + 1);
+  };
+
+  const handleBack = () => {
+    setStep((prevStep) => prevStep - 1);
+  };
+
+  switch (step) {
+    case 1:
+      buttons = (
+        <>
+          <Button onClick={handleCancel}>Cancel</Button>
+          <Button onClick={handleNext}>Next</Button>
+        </>
+      );
+      break;
+    case 5:
+      buttons = (
+        <>
+          <Button onClick={handleBack}>Back</Button>
+          <Button onClick={handleSubmit}>Submit</Button>
+        </>
+      );
+      break;
+    default:
+      buttons = (
+        <>
+          <Button onClick={handleBack}>Back</Button>
+          <Button onClick={handleNext}>Next</Button>
+        </>
+      );
+      break;
+  }
   return (
     <div>
-      <ProgressBar />
-      <div>registerWebinar</div>
+      <ProgressBar step={step} />
+      <div className={styles.RegisterWebinar}>
+        <div className={styles.ImageContainer}>
+          <img src={image} alt='register-webinar' />
+        </div>
+        <div>
+          {/* form */}
+          <div className={styles.ButtonContainer}>{buttons}</div>
+        </div>
+      </div>
     </div>
   );
 };
